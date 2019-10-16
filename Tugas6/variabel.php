4fbb8254c2e2e2a078b5abdf042c32d6914b7e57
rendraadjip <55478169+rendraadjip@users.noreply.github.com>
2019-10-16 21:57:34 +0700

    Add files via upload

---
+++ Tugas6/variabel.php
@@ -0,0 +1,22 @@
+<!-- pembuat kode php -->
+<?php 
+// penulisan variable dan deklarasi variabel
+$nama = "Andi";//mendeklarasi variabel nama
+$umur = "b17";//mendeklarasi variabel umur
+$a='Saya Sedang belajar PHP';//mendeklarasi variabel a
+$b=5;//mendeklarasi variabel b
+// menampilkan hasil dari variabel yang di deklarasikan
+echo $nama;
+echo "<br>";
+echo $umur;
+echo "<br>";
+
+print $a;
+echo $b;
+// mendeklarsi variable $andi
+$andi="Andi";
+echo $Andi; // Notice: Undefined variable: Andi
+//mengapa error karena karakter pada variable yang di deklarasikan adalah a kecil bukan A besar
+
+?>
+<!-- akhiran dari script php -->
